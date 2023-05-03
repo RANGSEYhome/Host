3d6388192fca4185c9437bd1350c55a331aedac5
Rangsey P. HENG <rangseyheng@puc.edu.kh>
2023-05-03 23:44:49 +0700

    Update script.js

--- bs/admin/script.js
+++ bs/admin/script.js
@@ -4,7 +4,39 @@
 
 /* <script> */
 
-  
+
+/* Disable page title */
+const pageTitle = document.querySelectorAll('.page .post-header .post-title');
+for (let i = 0; i < pageTitle.length; i++) {
+  pageTitle[i].style.display = 'none';
+}
+
+/* Hide lebel count and delete label prefix */
+// Hide count value
+const spansPostValue = document.querySelectorAll('.post-label span.label-count-value');
+for (let i = 0; i < spansPostValue.length; i++) {
+  spansPostValue[i].style.display = 'none';
+}
+// Hide arrow
+const spansPostArrow = document.querySelectorAll('.post-label span.label-count-arrow');
+for (let i = 0; i < spansPostArrow.length; i++) {
+  spansPostArrow[i].style.display = 'none';
+}
+// Fix label margin
+const aPostLabel = document.querySelectorAll('a.post-label');
+for (let i = 0; i < aPostLabel.length; i++) {
+  aPostLabel[i].style.margin= '12px -5px 0 0';
+}
+/* Delete label prefix */
+const spansLabelName = document.querySelectorAll('.bg.label-name');
+spansLabelName.forEach(span => {
+  const text = span.textContent.split(':')[1];
+  if (text) {
+    const trimmedText = text.trim();
+    span.textContent = trimmedText;
+  }
+});
+
 /* Fix copy pre text */
 // Change text (click to copy)
 window.addEventListener('load', function() {
@@ -30,34 +62,6 @@ function copyPreToClipboard(id) {
 		element.style.display = 'none';
 	});
 }
-
-
-/* Hide lebel count */
-// Hide count value
-const spansPostValue = document.querySelectorAll('.post-label span.label-count-value');
-for (let i = 0; i < spansPostValue.length; i++) {
-  spansPostValue[i].style.display = 'none';
-}
-// Hide arrow
-const spansPostArrow = document.querySelectorAll('.post-label span.label-count-arrow');
-for (let i = 0; i < spansPostArrow.length; i++) {
-  spansPostArrow[i].style.display = 'none';
-}
-// Fix label margin
-const aPostLabel = document.querySelectorAll('a.post-label');
-for (let i = 0; i < aPostLabel.length; i++) {
-  aPostLabel[i].style.margin= '12px -5px 0 0';
-}
-
-/* Delete label prefix */
-const spansLabelName = document.querySelectorAll('.bg.label-name');
-spansLabelName.forEach(span => {
-  const text = span.textContent.split(':')[1];
-  if (text) {
-    const trimmedText = text.trim();
-    span.textContent = trimmedText;
-  }
-});
 
 
 /* </script> */
