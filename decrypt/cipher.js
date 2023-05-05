2215c335b59e23aa3b2aa987c93d60dbe7112809
Rangsey P. HENG <rangseyheng@puc.edu.kh>
2023-05-05 20:22:38 +0700

    Update cipher.js

--- decrypt/cipher.js
+++ decrypt/cipher.js
@@ -1,1 +1,45 @@
+function htmlCipherEncrypt(str){ 
+  let strAm = "ABCDEFGHIJKLMNOPQRSTUVWXYZ abcdefghijklmnopqrstuvwxyz 0123456789 `-=~!@#$%^&*()_+[]\{}|;’:“”,./<>?";
+  let strNz = "IFGHABCDEPQRSTYJKLMNOZUVWX ifghabcdepqrstyjklmnozuvwx 2468097531 `-=~!@#$%^&*()_+[]\{}|;’:“”,./<>?";
+  let encrypt = '';
+  let htmlTag = false;
+  
+  for (let i = 0; i < str.length; i++){
+    if (str.charAt(i) === '<') {
+      htmlTag = true;
+    } else if (str.charAt(i) === '>') {
+      htmlTag = false;
+    }
+    
+    if (!htmlTag && strAm.includes(str.charAt(i))){
+      encrypt += str.charAt(i).replace(str.charAt(i), strNz[strAm.indexOf(str.charAt(i))]);
+    } else {
+      encrypt += str.charAt(i);
+    }
+  }
+  
+  return encrypt;
+}
 
+function htmlCipherDecrypt(str){ 
+  let strAm = "ABCDEFGHIJKLMNOPQRSTUVWXYZ abcdefghijklmnopqrstuvwxyz 0123456789 `-=~!@#$%^&*()_+[]\{}|;’:“”,./<>?";
+  let strNz = "IFGHABCDEPQRSTYJKLMNOZUVWX ifghabcdepqrstyjklmnozuvwx 2468097531 `-=~!@#$%^&*()_+[]\{}|;’:“”,./<>?";
+  let decrypt = '';
+  let htmlTag = false;
+  
+  for (let i = 0; i < str.length; i++){
+    if (str.charAt(i) === '<') {
+      htmlTag = true;
+    } else if (str.charAt(i) === '>') {
+      htmlTag = false;
+    }
+    
+    if (!htmlTag && strNz.includes(str.charAt(i))){
+      decrypt += str.charAt(i).replace(str.charAt(i), strAm[strNz.indexOf(str.charAt(i))]);
+    } else {
+      decrypt += str.charAt(i);
+    }
+  }
+  
+  return decrypt;
+}
